de1efdc90fb9759400bb9d1d1188e90b0fd993fc
Kem2523 <krystalemorris@gmail.com>
2022-10-02 12:58:28 -0500

    add bootstrap and images

--- controllers/places.js
+++ controllers/places.js
@@ -7,13 +7,13 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/cbr.JPG'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/loaded.JPG'
       }]
       
       res.render('places/index', { places })
--- views/error404.jsx
+++ views/error404.jsx
@@ -5,6 +5,10 @@ function error404 () {
     return (
       <Def>
           <main>
+          <img height="300" width="500" src="/images/404.jpg" alt="404" />
+      <div>
+        Photo by <a href="AUTHOR_LINK">Charles Deluvio</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+      </div>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
